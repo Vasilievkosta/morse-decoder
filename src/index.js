6add9d82e480750550ead7d5bb1647cdd987839c
Konstantin <vasilievkosta@gmail.com>
2020-10-18 22:57:52 +0300

    refactor: modified src

--- src/index.js
+++ src/index.js
@@ -38,7 +38,55 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+	let summary = '';	
+	for (let i = 0; i < expr.length; i++) {
+		if(i % 10 === 0 && i !== 0){
+			result += ',';
+		}
+		result += expr[i];		
+	}		
+		result = result.split(',');		
+		for (let i = 0; i < result.length; i++){
+			if (result[i] === '**********'){
+				summary += ' ';
+			} else {
+			summary += oneLetter(result[i]);
+			}
+		}		
+		return summary;
+	}	
+// function finde letter of 10 number
+	function oneLetter(expr) {
+	let codeLetter = '';
+	let codeMorse = '';
+	let index;
+	let letter = '';	
+	for (let i = expr.length - 1; i >= 0; i--) {		
+		if (expr[i] === '0' && expr[i-1] === '0' && expr[i+1] === '1'){
+			index = (i+1);					
+		}			
+	}
+	codeLetter = expr.slice(-(expr.length-index));
+	for (let i = 0; i < codeLetter.length; i++) {
+		if(i % 2 === 0 && i !== 0){
+			codeMorse += ',';
+		}
+		codeMorse += codeLetter[i];		
+	}	
+	codeMorse = codeMorse.split(',');
+	for(let i = 0; i < codeMorse.length; i++){
+		if(codeMorse[i] === '10') {
+			codeMorse[i] = '.';
+		} else {
+			codeMorse[i] = '-';
+		}
+	}
+	codeMorse = codeMorse.join('');	
+	for(let key in MORSE_TABLE) {
+		if(codeMorse === key) letter = MORSE_TABLE[key];
+	}	
+	return letter;
 }
 
 module.exports = {
